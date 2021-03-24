bd6448a6e7f24843704a856ceefd00c81b2db84c
Chinapanda <kittikornchina2014@gmail.com>
2021-03-24 17:45:24 +0700

    only run command

--- Assignment 2/RunOpenGL.bat
+++ Assignment 2/RunOpenGL.bat
@@ -1,21 +1,7 @@
-REM How to Use this bat file 
-REM Follow step by step below
-:: Install or Download MingGW and save it in a specific folder
-:: Download Freeglut and config it below
-::   -copy include and bin folder into MinGW
-::   -go to "bin folder" and copy freeglut.dll in to System32 and SysWOW64
-:: Extract the bat.(zip) and copy it into your folder
-:: set your project name like this "main.c"
-:: Run This.bat 
-:: Done
-
-
 REM
 echo "Running \m"
 gcc -c -o Test1.o plane1_base.cpp -I\"C:\Compiler\mingw64\x86_64-w64-mingw32\include" -I"C:\Libraries\gmtl-0.6.1"
-cls
 echo "Compiling Graphic"
-g++ -o Test1.exe  Test1.o -L\"C:/Compiler/mingw64/x86_64-w64-mingw32/lib" -lopengl32 -lglu32 -lfreeglut
-cls
+g++ -o Test1.exe  Test1.o -L\"C:\Compiler\mingw64\x86_64-w64-mingw32\lib" -lopengl32 -lglu32 -lfreeglut
 echo "App Running"
 Test1.exe
